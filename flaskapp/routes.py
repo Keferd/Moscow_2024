5436d3e6d94cc75b56f9f28ca7614418b43d8e90
Keferd <81792991+Keferd@users.noreply.github.com>
2024-04-28 00:03:27 +0500

    rd

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -16,91 +16,11 @@ def index():
 @app.route('/api/link', methods=['POST'])
 def post_link():
     try:
-        text = request.json['link_vacancy']
+        link = request.json['link_vacancy']
 
-        if text:
-            
-
-            response_data = {
-                "skills": {
-                    0: "Python",
-                    1: "SQL",
-                    2: "Django",
-                    3: "JavaScript",
-                    4: "HTML/CSS",
-                    5: "Git"
-                },
-                "courses": {
-                0: {
-                    "name": "Python-разработчик: быстрый старт в профессии",
-                    "link":"https://gb.ru/geek_university/developer/programmer/python-gb",
-                    "description": "Научитесь использовать язык программирования Python для создания сайтов, приложений, чат-ботов, нейросетей и даже для научных исследований",
-                    "duration": "9",
-                    "price": "6657",
-                    "accuracy": "87",
-                    "formats": {
-                        0: "Занятие в группе c преподавателем",
-                        1: "Онлайн-встречи c экспертами",
-                        2: "Онлайн-лекции и вебинары",
-                        3: "Видеозаписи занятий",
-                        4: "Практические занятия",
-                        5: "Домашняя работа"
-                    },
-                    "skills": {
-                        0: "Python",
-                        1: "SQL",
-                        2: "Django",
-                        3: "JavaScript",
-                        4: "HTML/CSS",
-                        5: "Git"
-                    }
-                },
-                1: {
-                    "name": "Программист c нуля до Junior",
-                    "link":"https://gb.ru/geek_university/developer/programmer",
-                    "description": "Выберите профессию в программировании в процессе обучения и станьте специалистом уровня Junior c зарплатой от 80 000 ₽",
-                    "duration": "12",
-                    "price": "5251",
-                    "accuracy": "68",
-                    "formats": {
-                        0: "Занятие в группе c преподавателем",
-                        1: "Онлайн-встречи c экспертами",
-                        2: "Онлайн-лекции и вебинары",
-                        3: "Видеозаписи занятий",
-                        4: "Практические занятия"
-                    },
-                    "skills": {
-                        0: "Python",
-                        1: "SQL",
-                        2: "JavaScript",
-                        3: "HTML/CSS",
-                        4: "Git"
-                    }
-                },
-                2: {
-                    "name": "Разработчик искусственного интеллекта",
-                    "link":"https://gb.ru/geek_university/developer/programmer/ai-spec",
-                    "description": "Научитесь c нуля создавать и управлять системами c использованием искусственного интеллекта",
-                    "duration": None,
-                    "price": "5999",
-                    "accuracy": "53",
-                    "formats": {
-                        0: "Занятие в группе c преподавателем",
-                        1: "Онлайн-встречи c экспертами",
-                        2: "Онлайн-лекции и вебинары"
-                    },
-                    "skills": {
-                        0: "Python",
-                        1: "SQL",
-                        2: "HTML/CSS",
-                        3: "Git"
-                    }
-                }
-                }
-            }
-
+        if link:
             return jsonify(response_data)
-
+        
         else:
             return "Ой-йой, что-то пошло не так", 400
 
