a29cfbcec8b35667a8e1dbe432ecc7ad6767a804
Keferd <81792991+Keferd@users.noreply.github.com>
2024-04-28 00:07:14 +0500

    123

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,11 +1,9 @@
 import os
 
 from flaskapp import app
-import pandas as pd
 from flask import render_template, make_response, request, Response, jsonify, json, session, redirect, url_for, send_file
-import functools
 import json
-
+from utils import get_json_data
 
 
 @app.route('/')
@@ -21,12 +19,12 @@ def post_link():
         if url:
             response_data = get_json_data(url)
             return response_data
-        
+
         else:
             return "Ой-йой, что-то пошло не так", 400
 
     except Exception as e:
-        print(e)
+        print("error:", e)
         return str(e), 500
 
 def json_response(data, code=200):
